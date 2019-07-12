6eb2db73e30164fc5ada1e759614af6120ab6e45
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-07-12 15:41:04 +0800

    Modified ml_sklearn.ipynb & made a few amendments

--- server/run.py
+++ server/run.py
@@ -1,6 +1,6 @@
 from main import app
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(debug=True, host='192.168.1.197', port=5000)
+    app.run(debug=True)
+    # app.run(debug=True, host='192.168.1.197', port=5000)
     # app.run(debug=True, host='172.18.168.209', port=5000)
